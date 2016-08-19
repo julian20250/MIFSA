16e8cf8ccfa080e56710a321d11bf8f21ff1bdba
julian <juojimenezca@unal.edu.co>
2016-08-19 13:29:42 -0500

    Versión de Califa actualizada a Python3

--- califa.py
+++ califa.py
@@ -1,11 +1,11 @@
-print "\n"
-print "Importing Libraries..."
+print("\n")
+print("Importing Libraries...")
 import sys
+from tkinter import filedialog as tkFileDialog
 if sys.version_info[0] < 3:
     from Tkinter import *
 else:
     from tkinter import *
-import tkFileDialog
 import os
 import continuum
 import numpy as np
@@ -16,7 +16,7 @@ import astropy.io.fits as pyfits
 import matplotlib.cm as cm
 import warnings
 import matplotlib.colorbar as cbar
-print "Importing Libraries... Done."
+print("Importing Libraries... Done.")
 
 #warnings.filterwarnings('ignore')
 
@@ -73,28 +73,28 @@ def showspectra(event):
         ax2.set_xlabel(x_label_spectra)
         ax2.set_ylabel("Flux")
         if token==0:
-            ax2.plot(wavelength, [data[cout][x][y] for cout in xrange(0,len(data),step)])
-            wav1,dat1=continuum.interpolate(wavelength,[data[cout][x][y] for cout in xrange(0,len(data),step)])
+            ax2.plot(wavelength, [data[cout][x][y] for cout in range(0,len(data),step)])
+            wav1,dat1=continuum.interpolate(wavelength,[data[cout][x][y] for cout in range(0,len(data),step)])
             ax2.plot(wav1,dat1)
         else:
-            ax2.plot(wavelength, [data[cout][x][y] for cout in xrange(begin_wl,end_wl,step)])
-            wav1,dat1=continuum.interpolate(wavelength,[data[cout][x][y] for cout in xrange(begin_wl,end_wl,step)])
+            ax2.plot(wavelength, [data[cout][x][y] for cout in range(begin_wl,end_wl,step)])
+            wav1,dat1=continuum.interpolate(wavelength,[data[cout][x][y] for cout in range(begin_wl,end_wl,step)])
             ax2.plot(wav1,dat1)
-        print "Drew spectra of pixel (%i, %i)"%(x,y)
+        print("Drew spectra of pixel (%i, %i)"%(x,y))
         f.canvas.draw()
         preserve_x,preserve_y=x,y
     except:
       x,y=event.xdata, event.ydata
       if (x!=None and y!=None):
-	ax2.clear()
-	f.canvas.draw()
+        ax2.clear()
+        f.canvas.draw()
         ran=emergent_window(1)
         ax2.set_xlim(ran[0],ran[1])
         wally, biggy, endy =compare(wavelength,ran,1)
-        ax2.plot(wally, [data[cout][preserve_x][preserve_y] for cout in xrange(biggy,endy,1)])
+        ax2.plot(wally, [data[cout][preserve_x][preserve_y] for cout in range(biggy,endy,1)])
         f.canvas.draw()
       else:
-	pass
+          pass
 
 def end(root):
     root.destroy()
@@ -105,14 +105,14 @@ def compare(wavelength,ran,step):
     compare_end=[abs(x-ran[1]) for x in wavelength]
     begin_wl=compare_begin.index(min(compare_begin))
     end_wl=compare_end.index(min(compare_end))
-    wavelength=[wavelength[x] for x in xrange(begin_wl,end_wl,step)]
+    wavelength=[wavelength[x] for x in range(begin_wl,end_wl,step)]
     return wavelength,begin_wl,end_wl
     
 supported_parameters=["-no_interval"]
 warnings.simplefilter(action = "ignore", category= FutureWarning)
 warnings.simplefilter(action = "ignore", category= UserWarning)
 while True:
-    print "\nChecking Parameters..."
+    print("\nChecking Parameters...")
     if len(sys.argv)>1:
         for x in sys.argv[1:]:
             flagger=0
@@ -122,31 +122,31 @@ while True:
             if flagger==0:
                 os.system("cat usage.txt")
                 sys.exit()
-    print "Checking Parameters... Done."
+    print("Checking Parameters... Done.")
 
     #Steps of spectra
     step=2
 
     #Opening File and Pointing
-    print "\nOpening FITS file..."
+    print("\nOpening FITS file...")
     #s = pyfits.open("IC1683.V500.rscube.fits.gz")
     s= pyfits.open(choose().name)
-    print "About the File:"
-    print "==================="
+    print("About the File:")
+    print("===================")
     s.info()
-    print "==================="
+    print("===================")
     data= s[0].data
     headers = s[0].header
 
     #Getting Headers of The file
     header_list=[]
     header_result=[]
-    for x, y in headers.iteritems():
+    for x, y in headers.items():
         header_list.append(x)
         header_result.append(y)
-    print "Opening FITS file... Done."
+    print ("Opening FITS file... Done.")
     #Finding the format of the data
-    print "\nGetting Headers of the FITS file..."
+    print("\nGetting Headers of the FITS file...")
     count=0
     for x in header_list:
         if "CUNIT3" in x:
@@ -174,23 +174,23 @@ while True:
     RA=[]
     count=dec_value-dec_step*dec_ref
 
-    for x in xrange(1, len(data[0][0])):
+    for x in range(1, len(data[0][0])):
         declination.append(count)
         count+=dec_step
     count=ra_value-ra_step*dec_ref
-    for x in xrange(1, len(data[0])):
+    for x in range(1, len(data[0])):
         RA.append(count)
         count+=ra_step
 
     declination= [str(int(x))+"deg"+str(int((x-int(x))*60.))+"'"+str(int(((x-int(x))*60.-int((x-int(x))*60.))*60.))+"''" for x in declination]
     RA=[str(int(x*24/360.))+"h"+str(int((x*24/360.-int(x*24/360.))*60))+"m"+"%1.2f"%(((x*24/360.-int(x*24/360.))*60-int((x*24/360.-int(x*24/360.))*60))*60)+"s" for x in RA]
-    print "Getting Headers of the FITS file... Done."
+    print("Getting Headers of the FITS file... Done.")
 
     #print x_label_spectra, lambda_begin, lambda_step, lambda_pix
     #Adquire the wavelength interval
-    print "\nSetting Wavelength..."
+    print("\nSetting Wavelength...")
     wavelength= []
-    for x in xrange(int(lambda_begin), int(lambda_begin+lambda_step*len(data)), int(lambda_step)):
+    for x in range(int(lambda_begin), int(lambda_begin+lambda_step*len(data)), int(lambda_step)):
         wavelength.append(x)
     #Emergent Window
     ran=None
@@ -207,26 +207,26 @@ while True:
     if ran != None:
         wavelength,begin_wl,end_wl=compare(wavelength,ran,step)
         token=1
-        for x in xrange(len(data[0])):
-            for y in xrange(len(data[0][0])):
-                for z in xrange(0,len(data),step):
+        for x in range(len(data[0])):
+            for y in range(len(data[0][0])):
+                for z in range(0,len(data),step):
                     if begin_wl<=z<=end_wl:
                         pass
                     else:
                         data[z][x][y]=0
     else:
-        wavelength = [wavelength[x] for x in xrange(0,len(data),step)]
+        wavelength = [wavelength[x] for x in range(0,len(data),step)]
         pass
 
-    print "Setting Wavelength... Done."
+    print("Setting Wavelength... Done.")
 
 
     #Setting Pixels
-    print "\nSetting Pixels..."
+    print("\nSetting Pixels...")
     magnitude_black=[]
-    for x in xrange(len(data[0])):
-        for y in xrange(len(data[0][0])):
-            magnitude_black.append(sum([data[cout][x][y] for cout in xrange(0,len(data),step)]))
+    for x in range(len(data[0])):
+        for y in range(len(data[0][0])):
+            magnitude_black.append(sum([data[cout][x][y] for cout in range(0,len(data),step)]))
     max_color=max(magnitude_black)
     min_color=min(magnitude_black)
     magnitude_black=[(x-min_color)/(max_color-min_color) for x in magnitude_black]
@@ -236,27 +236,27 @@ while True:
     #Another new Part
     my_cmap= cm.get_cmap("spectral") #Here to Change Colorbar
     color_data= my_cmap(np.array(magnitude_black))
-    print "Setting Pixels... Done."
+    print("Setting Pixels... Done.")
     #Graphic the environment
-    print "\nSetting Environment..."
+    print("\nSetting Environment...")
     f, (ax1, ax2) = plt.subplots(1,2, figsize=(15,10)) #If you have problems in plotting of the data,
     #remove the figsize arg
     count=0
 
-    for x in xrange(len(data[0])):
-        for y in xrange(len(data[0][0])):
+    for x in range(len(data[0])):
+        for y in range(len(data[0][0])):
             ax1.add_patch(patches.Rectangle((y-.5,x-.5),1,1,fill=True, edgecolor="black", color=color_data[count]))
             count+=1
     ax1.set_xlim(0,len(data[0][0])-1)
     ax1.set_ylim(0,len(data[0])-10)
-    ax1.set_yticks(xrange(0,len(data[0][0])-10, 6))
-    ax1.set_yticklabels([declination[x] for x in xrange(0,len(declination),6)])
+    ax1.set_yticks(range(0,len(data[0][0])-10, 6))
+    ax1.set_yticklabels([declination[x] for x in range(0,len(declination),6)])
     normal = plt.Normalize(min(magnitude_black), max(magnitude_black))
     cax, _ = cbar.make_axes(ax1)
     cb2 = cbar.ColorbarBase(cax, cmap=my_cmap,norm=normal)
 
-    ax1.set_xticks(xrange(0, len(data[0])-1, 4))
-    ax1.set_xticklabels([RA[x] for x in xrange(0,len(RA),4)], rotation=90)
+    ax1.set_xticks(range(0, len(data[0])-1, 4))
+    ax1.set_xticklabels([RA[x] for x in range(0,len(RA),4)], rotation=90)
     ax1.set_ylabel("Declination")
     ax1.set_xlabel("RA")
     plt.suptitle(headers["OBJECT"])
@@ -265,18 +265,18 @@ while True:
     ax1.format_coord = lambda x,y : "Pixel: (%0g, %0g)"%(x, y)
     ax2.format_coord = lambda x,y : "Wavelength=%0g, Flux=%0g"%(x, y)
     cid = f.canvas.mpl_connect('button_press_event', showspectra)
-    print "Setting Environment... Done."
+    print("Setting Environment... Done.")
 
     #Showing the environment
-    print "\nShowing Environment..."
+    print("\nShowing Environment...")
     plt.show()
     root=Tk()
     root.geometry("500x500")
     Button(root,text="End",command=lambda: end(root)).grid()
     Button(root,text="Continue", command=root.destroy).grid()
     root.mainloop()
-    print "Finishing Process."
-    print "\n\n"
+    print("Finishing Process.")
+    print("\n\n")
 
 #http://matplotlib.org/users/event_handling.html
 #http://docs.scipy.org/doc/numpy-1.10.0/reference/generated/numpy.polyfit.html
