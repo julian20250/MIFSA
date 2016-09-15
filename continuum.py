9a45261eae4244eb691671cde069fbd231220e7c
julian <juojimenezca@unal.edu.co>
2016-09-14 21:35:13 -0500

    Changed polynomial fit

--- continuum.py
+++ continuum.py
@@ -1,11 +1,12 @@
 import numpy as np
 def interpolate(wavelength, flux):
     "This function makes the interpolation and return the data to be plotted"
-    n=3 #order of polynom
+    #order of polynom
     wavelength,flux=np.array(wavelength), np.array(flux)
     dev=np.std(flux)
     index=(np.absolute(flux-flux.mean())<dev) & (flux<flux.mean())
     wave1=wavelength[index]
+    n=5
     flux1=flux[index]
     coeficients=np.polyfit(wave1,flux1,n)
     data=np.polyval(coeficients, wave1)
